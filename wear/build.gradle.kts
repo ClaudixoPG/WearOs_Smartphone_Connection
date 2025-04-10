0c4f70aea0cc2e766dee58b059e3d0d1210623c3
Claudio Rubio <43611630+ClaudixoPG@users.noreply.github.com>
2025-04-10 16:59:06 -0400

    Initial commit

---
+++ wear/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.randomadjective.prototipodatalayer"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.randomadjective.prototipodatalayer"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.wear)
+    implementation(libs.play.services.wearable)
+    implementation("androidx.wear:wear:1.2.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+
+}
---
+++ wear/src/main/java/com/randomadjective/prototipodatalayer/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.randomadjective.prototipodatalayer
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.*
+import java.nio.charset.StandardCharsets
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : ComponentActivity(), MessageClient.OnMessageReceivedListener {
+
+    private lateinit var textView: TextView
+    private lateinit var inputMessage: EditText
+    private lateinit var sendButton: Button
+    private val path = "/mensaje"
+    private val TAG = "Wear_Main"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textViewWear)
+        inputMessage = findViewById(R.id.inputMessageWear)
+        sendButton = findViewById(R.id.buttonSendWear)
+
+        sendButton.setOnClickListener {
+            val mensaje = inputMessage.text.toString()
+            enviarMensajeAlTelefono(mensaje)
+        }
+
+        enviarMensajeAlTelefono("Mensaje enviado desde smartwatch")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Wearable.getMessageClient(this).addListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Wearable.getMessageClient(this).removeListener(this)
+    }
+
+    override fun onMessageReceived(event: MessageEvent) {
+        if (event.path == path) {
+            val mensaje = String(event.data, StandardCharsets.UTF_8)
+            Log.i(TAG, "Mensaje recibido: $mensaje")
+            runOnUiThread {
+                textView.text = "Recibido: $mensaje"
+            }
+        }
+    }
+
+    private fun enviarMensajeAlTelefono(mensaje: String) {
+        Thread {
+            val nodeListTask = Wearable.getNodeClient(this).connectedNodes
+            val nodes = Tasks.await(nodeListTask)
+            for (node in nodes) {
+                Wearable.getMessageClient(this)
+                    .sendMessage(node.id, path, mensaje.toByteArray())
+                Log.i(TAG, "Mensaje enviado a ${node.displayName}")
+            }
+        }.start()
+    }
+}
